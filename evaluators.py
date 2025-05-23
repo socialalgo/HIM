202bbf34a1466cc6f379d68ffa7753b9fc82e373
hongru <110719204+hongruhou89@users.noreply.github.com>
2025-05-23 14:28:56 +0800

    Update evaluators.py

--- evaluators.py
+++ evaluators.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 def spread_num_process():
 
-    spread_Data = pd.read_csv("datasets/twitter_spread.csv")
+    spread_Data = pd.read_csv("data/twitter_spread.csv")
     spread = np.array(spread_Data)
 
     spread_num = defaultdict(int)
@@ -25,7 +25,7 @@ def Theory_maximum_rec():
 
     Kmax = 3
 
-    spread_Data = pd.read_csv("datasets/twitter_spread.csv")
+    spread_Data = pd.read_csv("data/twitter_spread.csv")
 
     col0 = spread_Data.columns[0]
     col1 = spread_Data.columns[1]
@@ -48,7 +48,6 @@ def Theory_maximum_rec():
             spread_dict[src].append(dst)
             spread_num[src] = spread_num[src] + 1
 
-    # 记录所有用户spread@K的理论上界
     for user in spread_dict:
         upper_lim[user] = [-1 for _ in range(Kmax)]
         upper_user[user] = [[] for _ in range(Kmax)]
@@ -95,10 +94,10 @@ def ISpread_K_calculated():
     ISpread_K = []
 
     # test nodes
-    ap_file = "datasets/ap_list_spread_final.csv"
+    ap_file = "data/ap_list_spread_final.csv"
     test_node = [_ for _ in np.array(pd.read_csv(ap_file, header=None)).reshape(-1, )]
 
-    spread_Data = pd.read_csv("datasets/twitter_spread.csv")
+    spread_Data = pd.read_csv("data/twitter_spread.csv")
 
     upper_limit_user = Theory_maximum_rec()
     print("Theory recommend list generated!")
@@ -136,9 +135,9 @@ def ISpread_K_calculated():
             else:
                 rec_list.extend(theory_upper[limit_dict[node] - 1])
 
-        rec_list = np.unique(rec_list)  # 去重
+        rec_list = np.unique(rec_list)  # deduplicate
         second_coverage = []
-        second_coverage.extend(rec_list)    # 一阶传播加进去
+        second_coverage.extend(rec_list)    # extend first-order spread from inviter to invitees
         for rec in rec_list:
             second_coverage.extend(spread_dict[rec])
         print("ISpread@{} = {}".format(K, len(np.unique(second_coverage))))
@@ -146,18 +145,18 @@ def ISpread_K_calculated():
         ISpread_K.append(len(np.unique(second_coverage)))
 
     df = pd.DataFrame(ISpread_K)
-    df.to_csv("datasets/ISpread@K.csv", index=False, header=False)
+    df.to_csv("data/ISpread@K.csv", index=False, header=False)
 
 def evaluate(Recommendation):
 
     print("Evaluation Begins")
 
     # Get idmap
-    with open("datasets/id2uid.json", "r") as fidr:
+    with open("data/id2uid.json", "r") as fidr:
         idmap = json.load(fidr)
 
     # network & probability predicted by EulerNet
-    edge_list = np.array(pd.read_csv("datasets/edge_list.csv", header=None))
+    edge_list = np.array(pd.read_csv("data/edge_list.csv", header=None))
     frienddict = defaultdict(list)
     for src, dst, p in edge_list:
         frienddict[idmap[str(int(src))]].append(idmap[str(int(dst))])
@@ -165,11 +164,10 @@ def evaluate(Recommendation):
     for uid in frienddict:
         friendnum[uid] = len(frienddict[uid])
 
-    # spread_Data是原始uid
-    spread_Data = pd.read_csv("datasets/twitter_spread.csv")
+    spread_Data = pd.read_csv("data/twitter_spread.csv")
 
     # test node file
-    ap_file = "datasets/ap_list_spread_final.csv"
+    ap_file = "data/ap_list_spread_final.csv"
     test_left_node = [idmap[str(_)] for _ in np.array(pd.read_csv(ap_file, header=None)).reshape(-1, )]
     test_node_dict = dict()
     for node in test_left_node:
@@ -191,14 +189,13 @@ def evaluate(Recommendation):
 
     test_data = Recommendation
 
-    # 对每个用户排序
     test_user_dict = defaultdict(list)
 
     for src, dst, v in tqdm(test_data):
         src = int(src)
         dst = int(dst)
         if src in test_node_dict:
-            test_user_dict[src].append([dst, v])    # 取出给test_node的推荐结果
+            test_user_dict[src].append([dst, v])    # get the recommendation results
 
     # Recall@K, NDCG@k
     K = 3
@@ -206,14 +203,13 @@ def evaluate(Recommendation):
     DCG_K = [[0 for __ in range(len(test_user_dict))] for _ in range(K)]
     NDCG_K = [[] for _ in range(K)]
 
-    Spread_K_valid = dict()   # 记录推荐出来的前K个人的有效传播轨迹（一阶传播）
+    Spread_K_valid = dict()   # recommend the valid recommendation in TopK
     for user in test_user_dict:
         Spread_K_valid[user] = [[], [], []]
 
-    # 记录每个用户的recall@K(1, 2, 3)
     Recall_K_deg = defaultdict(list)
 
-    # 预先算好每个用户的idcg值
+    # predefine the idcg@K of each user
     idcg_data = defaultdict(list)   # n*3, [[u1_idcg@1, u1_idcg@2, u1_idcg@3], ...[un_idcg@1, un_idcg@2, un_idcg@3]]
     idcg = [1/np.log2(2), 1/np.log2(2) + 1/np.log2(3), 1/np.log2(2) + 1/np.log2(3) + 1/np.log2(4)]  # init idcg
     for user in test_user_dict:
@@ -227,26 +223,24 @@ def evaluate(Recommendation):
         algo_rec = sorted(test_user_dict[user], key=lambda x: x[1], reverse=True)
         for _k in range(1, K + 1):
 
-            algo_k = np.array(algo_rec)[:_k, 0]  # 按序取出前_k个的uid
-            n_union = len(np.intersect1d(algo_k, spread_dict[user]))    # 计算交际
+            algo_k = np.array(algo_rec)[:_k, 0]  # get the recommendation list
+            n_union = len(np.intersect1d(algo_k, spread_dict[user]))    # compared with the ground truth
 
             Spread_K_valid[user][_k - 1].extend(np.intersect1d(algo_k, spread_dict[user]).tolist())
 
             Recall_K[_k - 1].append(n_union / spread_num[user])
-            Recall_K_deg[user].append(n_union / spread_num[user])   # 记录每个用户的recall@K
+            Recall_K_deg[user].append(n_union / spread_num[user])   # calculate the recall@K per user
 
             if _k == 1:
                 DCG_K[_k - 1][uid] = n_union / _k
                 NDCG_K[_k - 1].append(DCG_K[_k - 1][uid] / idcg_data[user][_k - 1])
             else:
-                # 第_k位是否被点击到
                 is_valid_k = (algo_k[_k - 1] in spread_dict[user])
                 DCG_K[_k - 1][uid] = DCG_K[_k - 2][uid] + is_valid_k/np.log2(_k + 1)
                 NDCG_K[_k - 1].append(DCG_K[_k - 1][uid] / idcg_data[user][_k - 1])
 
-    # 计算Spread@K
+    # Sperad@K
     Spread_K_full = [[] for _ in range(K)]
-    Spread_K_full_user = defaultdict(list) # 记录一下每个人的s@1, s@2, s@3, 再进一步筛一下测试集
 
     for user in Spread_K_valid:
         for _k in range(K):
@@ -257,17 +251,15 @@ def evaluate(Recommendation):
             first_order.extend(second_order)
             Spread_K_full[_k].extend(first_order)
 
-            Spread_K_full_user[user].append(len(first_order))
-
     recall_res = [np.mean(rk) for rk in Recall_K]
     ndcg_res = [np.mean(ndcg) for ndcg in NDCG_K]
     Spread_K_unique = [len(np.unique(spk)) for spk in Spread_K_full]
 
-    if os.path.exists("datasets/ISpread@K.csv") is not True:
+    if os.path.exists("data/ISpread@K.csv") is not True:
         print("Compute the ISpread@K")
         ISpread_K_calculated()
 
-    ISpread_K = np.array(pd.read_csv("datasets/ISpread@K.csv", header=None)).reshape(-1, )
+    ISpread_K = np.array(pd.read_csv("data/ISpread@K.csv", header=None)).reshape(-1, )
 
     print("Recall@1, 2, 3 = [{}, {}, {}]".format(recall_res[0], recall_res[1], recall_res[2]))
     print("NDCG@1, 2, 3 = [{}, {}, {}]".format(ndcg_res[0], ndcg_res[1], ndcg_res[2]))
@@ -277,10 +269,10 @@ def evaluate(Recommendation):
 
 if __name__ == '__main__':
 
-    EulerNet_data = pd.read_csv("datasets/edge_list.csv").values.tolist()
+    EulerNet_data = pd.read_csv("data/edge_list.csv").values.tolist()
     Recommendation = []
 
-    with open("datasets/id2uid.json", "r") as fr:
+    with open("data/id2uid.json", "r") as fr:
         idmap = json.load(fr)
 
     for src, tar, prob in EulerNet_data:
